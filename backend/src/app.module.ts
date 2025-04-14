aebe02c150682e02a344dae21588fa271c6cd264
eroncardoso88 <deandradeeron@gmail.com>
2025-04-14 17:13:47 -0300

    feat(`backend`): added todos module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
+// backend/src/app.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 import { DatabaseModule } from './database/database.module';
 import { RedisModule } from './redis/redis.module';
+import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -15,6 +18,7 @@ import { UsersModule } from './users/users.module';
     RedisModule,
     AuthModule,
     UsersModule,
+    TodosModule,
   ],
 })
 export class AppModule {}
