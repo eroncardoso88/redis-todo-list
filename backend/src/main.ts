14ddeaf237967c5805a13c86cca6cf72c6550df2
eroncardoso88 <deandradeeron@gmail.com>
2025-04-14 16:41:24 -0300

    feat(`backend`): enable cors for dev localhost

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4321', 
+    credentials: true,
+  });
+
   const configService = app.get(ConfigService);
   const port = configService.get('port');
   
