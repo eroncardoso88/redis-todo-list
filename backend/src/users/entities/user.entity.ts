8ebedd4b166f8576ec23a58e1721323c4ff8aa39
eroncardoso88 <deandradeeron@gmail.com>
2025-04-14 12:15:51 -0300

    feat(`backend`): user entity

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import * as bcrypt from 'bcrypt';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  async validatePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
+}
