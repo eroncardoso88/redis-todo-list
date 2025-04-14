b6ad07430dfcd1dc1e48a0904fc706ac75cd157c
eroncardoso88 <deandradeeron@gmail.com>
2025-04-14 12:02:03 -0300

    feat(`backend`): added modules configuration database and redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { DatabaseModule } from './database/database.module';
+import { RedisModule } from './redis/redis.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    DatabaseModule,
+    RedisModule,
   imports: [],
   controllers: [AppController],
   providers: [AppService],
+  ],
 })
 export class AppModule {}
